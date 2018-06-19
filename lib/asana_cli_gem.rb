dedf1f4a73df8cc9a0621e56f643d17c2852687d
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 11:39:36 +0000

    Update for use as pseudo-environment

--- lib/asana_cli_gem.rb
+++ lib/asana_cli_gem.rb
@@ -1,5 +1,3 @@
-require "asana_cli_gem/version"
-
-module AsanaCliGem
-  # Your code goes here...
-end
+require_relative './asana_cli_gem/version'
+require_relative './asana_cli_gem/cli'
+# using this file as a pseudo environment file to require all other dependencies
