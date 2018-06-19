009bf37ddc40c1cddc936b77e934ac65073a04a6
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 14:43:13 +0000

    Find scrape arguments for Asana object attributes

--- lib/asana_cli_gem/scraper.rb
+++ lib/asana_cli_gem/scraper.rb
@@ -6,8 +6,6 @@ class Scraper
 
   @@site = Nokogiri::HTML(open('https://www.yogajournal.com/poses/types/strength'))
 
-  # binding.pry
-
   def self.site
     @@site
   end
@@ -20,9 +18,22 @@ class Scraper
   end
 
   def self.pose_url_collector
+    # current scrape includes non-pose urls on the page too, need to get them out
     site.css('a.m-card--header').collect do |pose|
       pose.attribute("href").value
     end
+  end
+  binding.pry
+
+  def self.asana_generator
+    self.pose_url_collector.each do |pose|
+      pose = Nokogiri::HTML(open("https://www.yogajournal.com#{pose}"))
+      puts pose.css('div.m-detail-header--dek').text
+    end
+
+    # pose.url => pose = Nokogiri::HTML(open("https://www.yogajournal.com#{pose}"))
+    # pose.name => puts pose.css('h1').text
+
   end
 
 end
