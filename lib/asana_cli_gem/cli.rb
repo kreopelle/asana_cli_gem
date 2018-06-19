6e32b571021c44e5fb67d6faec1d5a8e701d9a46
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 12:32:37 +0000

    Add descriptive comments

--- lib/asana_cli_gem/cli.rb
+++ lib/asana_cli_gem/cli.rb
@@ -3,6 +3,7 @@
 class AsanaCliGem::CLI
 
   def call
+    # backbone of CLI, calls other methods (is there another name for this?)
     puts "Strengthening Yoga Poses:"
     list_poses
     menu
@@ -10,6 +11,7 @@ class AsanaCliGem::CLI
   end
 
   def list_poses
+    # where available asana objects will be listed for access
     puts <<~DOC
       1. Boat Pose
       2. Chair Pose
@@ -18,20 +20,27 @@ class AsanaCliGem::CLI
   end
 
   def menu
-    puts "Enter the number of the pose you'd like to learn more about, or type 'exit' to end program."
+    # user interaction interface
+    puts "Please enter the number of the pose you'd like to learn more about, type list to see all poses, or exit to end program."
     input = gets.strip.downcase
+    # normalizes text input
     if input != "exit"
       case input
       when "1"
         puts "More info on Boat Pose"
       when "2"
         puts "More info on Chair Pose"
+      when "list"
+        list_poses
+      else
+        puts "My apologies, I didn't recognize that input."
       end
       menu
     end
   end
 
   def goodbye
+    # exit message
     puts "Namaste"
   end
 
