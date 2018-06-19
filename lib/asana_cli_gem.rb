221b71596c7fc137d7b4079fcedd8ab6f249e768
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 13:07:15 +0000

    Require asana.rb

--- lib/asana_cli_gem.rb
+++ lib/asana_cli_gem.rb
@@ -1,3 +1,4 @@
 require_relative './asana_cli_gem/version'
 require_relative './asana_cli_gem/cli'
+require_relative './asana_cli_gem/asana'
 # using this file as a pseudo environment file to require all other dependencies
