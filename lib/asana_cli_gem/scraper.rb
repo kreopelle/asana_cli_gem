5f82714c3d48568ad39f2bfad281df43dcf520b7
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 14:24:49 +0000

    Create class variable to store site, update methods

--- lib/asana_cli_gem/scraper.rb
+++ lib/asana_cli_gem/scraper.rb
@@ -4,18 +4,23 @@ require 'pry'
 
 class Scraper
 
-  poses = Nokogiri::HTML(open('https://www.yogajournal.com/poses/types/strength'))
+  @@site = Nokogiri::HTML(open('https://www.yogajournal.com/poses/types/strength'))
 
-  binding.pry
+  # binding.pry
 
-  def pose_name_collector
-    poses.css('.m-card--header').collect do |pose|
+  def self.site
+    @@site
+  end
+
+  def self.pose_name_collector
+  # current scrape includes non-pose articles on the page too, need to get them out
+    site.css('.m-card--header').collect do |pose|
       pose.css('h2').text
     end
   end
 
-  def pose_url_collector
-    poses.css('a.m-card--header').collect do |pose|
+  def self.pose_url_collector
+    site.css('a.m-card--header').collect do |pose|
       pose.attribute("href").value
     end
   end
