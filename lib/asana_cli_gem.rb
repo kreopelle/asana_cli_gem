afe5a622490b6c654c30fc18bc7d9061d86c098b
Kayla Reopelle <kreopelle@gmail.com>
2018-06-19 14:23:50 +0000

    Add Scraper.rb to requirements

--- lib/asana_cli_gem.rb
+++ lib/asana_cli_gem.rb
@@ -1,4 +1,5 @@
 require_relative './asana_cli_gem/version'
 require_relative './asana_cli_gem/cli'
 require_relative './asana_cli_gem/asana'
+require_relative './asana_cli_gem/scraper'
 # using this file as a pseudo environment file to require all other dependencies
